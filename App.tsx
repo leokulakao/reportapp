086b2907f2a8b2a8d9637cd1428e0f47d00631b4
Arsen Kylnyk <arsen.kylnyk@galvintec.com>
2023-03-05 00:14:19 +0100

    init sentry

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as SplashScreen from 'expo-splash-screen';
+import * as Sentry from 'sentry-expo';
 
 import './localization';
 import store from './src/store/store';
@@ -13,6 +14,12 @@ import AnimatedAppLoader from './src/components/AnimatedAppLoader';
 import Constants from 'expo-constants';
 
 SplashScreen.preventAutoHideAsync();
+
+Sentry.init({
+  dsn: 'https://a4b07c8f5e8a44be8020fe42f3be08ae@o4504768446005248.ingest.sentry.io/4504768448823296',
+  enableInExpoDevelopment: true,
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 const App = () => {
   const persistor = persistStore(store);
